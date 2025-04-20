a83182602c25b8dfac12740b5128ceb7e161399e
NazishKiran <nazishkirancosmos123@gmail.com>
2025-04-20 15:51:55 +0500

    login for captain

--- Backend/controllers/captain.controller.js
+++ Backend/controllers/captain.controller.js
@@ -3,43 +3,79 @@ import { cookie, validationResult } from "express-validator";
 import userModel from "../models/user.model.js";
 
 export const registerCaptain = async (req, res) => {
-  const { firstname, lastname, email, password, socketId, vehicle, location } =
-    req.body;
-  if (!firstname || !lastname || !email || !password || !socketId || !vehicle) {
-    console.log("All fields are required");
+  try {
+    const {
+      firstname,
+      lastname,
+      email,
+      password,
+      socketId,
+      vehicle,
+      location,
+    } = req.body;
+    if (
+      !firstname ||
+      !lastname ||
+      !email ||
+      !password ||
+      !socketId ||
+      !vehicle
+    ) {
+      console.log("All fields are required");
+    }
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const existingCaptain = await captainModel.findOne({ email });
+    if (existingCaptain) {
+      return res.status(409).json({ message: "Email already registered" });
+    }
+
+    const hashedpassword = await userModel.hashPassword(password);
+
+    const captain = await captainModel.create({
+      firstname,
+      lastname,
+      email,
+      password: hashedpassword,
+      socketId,
+      vehicle: {
+        color: vehicle.color,
+        capacity: vehicle.capacity,
+        plate: vehicle.plate,
+        vehicleType: vehicle.vehicleType,
+      },
+      location: {
+        lat: location.lat,
+        lng: location.lng,
+      },
+    });
+
+    const token = captain.generateAuthToken();
+    res.cookie("token", token);
+    res.status(201).json({ token, captain });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
   }
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+};
+
+export const loginCaptain = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const captain = await captainModel.findOne(email).select("+password");
+
+    if (!captain) {
+      return res.status(401).json({ message: "incorrect email or password" });
+    }
+    const isMatch = await captain.comparePassword(password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "incorrect email or password" });
+    }
+    const token = captain.generateAuthToken();
+    res.cookie("token", token);
+    res.status(200).json({ captain, token });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
   }
-  const existingCaptain = await captainModel.findOne({ email });
-  if (existingCaptain) {
-    return res.status(409).json({ message: "Email already registered" });
-  }
-
-  const hashedpassword = await userModel.hashPassword(password);
-
-  const captain = await captainModel.create({
-    firstname,
-    lastname,
-    email,
-    password: hashedpassword,
-    socketId,
-    vehicle: {
-      color: vehicle.color,
-      capacity: vehicle.capacity,
-      plate: vehicle.plate,
-      vehicleType: vehicle.vehicleType,
-    },
-    location: {
-      lat: location.lat,
-      lng: location.lng,
-    },
-  });
-
-  const token = captain.generateAuthToken();
-
-  res.cookie("token", token);
-
-  res.status(201).json({ token, captain });
 };
